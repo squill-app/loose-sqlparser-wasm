36fced608cbeedfcb938db6d27f53c406742dbf5
Philippe Shampanier <pshampanier@gmail.com>
2024-09-03 12:24:51 -0700

    version 0.1.4 with interfaces

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-## Building the package
+# Usage
+
+```ts
+import { parseSql } from "loose-sqlparser-wasm";
+const statements = parseSql("SELECT 1; SELECT 2") as Array<Statement>;
+```
+
+# Building the package
 
 ```sh
 wasm-pack build --target bundler
+wasm-pack pack
+wasm-pack publish
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,72 @@
 use loose_sqlparser::{parse_with_options, Options, Statement};
-use serde_wasm_bindgen::from_value;
 use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn parse_sql(sql: &str, options: JsValue) -> Result<JsValue, JsValue> {
-    let options: Options = if options.is_undefined() || options.is_null() {
-        Options::default()
-    } else {
-        from_value(options)
-            .map_err(|err| JsValue::from_str(&format!("Invalid options: {}", err)))?
+#[wasm_bindgen(typescript_custom_section)]
+const TS_TYPES: &'static str = r#"
+export type TokenType =
+  | "Any"
+  | "Comment"
+  | "QuotedIdentifierOrConstant"
+  | "NumericConstant"
+  | "IdentifierOrKeyword"
+  | "Operator"
+  | "StatementDelimiter"
+  | "Fragment";
+
+export interface Token {
+  value: string | Array<Token>;
+  type: TokenType;
+  start: {
+    offset: number;
+    line: number;
+    column: number;
+  };
+  end: {
+    offset: number;
+    line: number;
+    column: number;
+  };
+}
+
+export interface Statement {
+  input: string;
+  tokens: Array<Token>;
+}
+"#;
+
+#[wasm_bindgen(js_name = Options)]
+pub struct JsOptions(Options);
+
+#[allow(clippy::new_without_default)]
+#[wasm_bindgen(js_class = Options)]
+impl JsOptions {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        JsOptions(Options::default())
+    }
+
+    #[wasm_bindgen(getter, js_name = statementDelimiter)]
+    pub fn statement_delimiter(&self) -> String {
+        self.0.statement_delimiter.clone()
+    }
+
+    #[wasm_bindgen(setter, js_name = statementDelimiter)]
+    pub fn set_statement_delimiter(&mut self, value: String) {
+        self.0.statement_delimiter = value;
+    }
+}
+
+/// Parses the given SQL string using the provided options.
+///
+/// @param {string} sql - The SQL string to parse.
+/// @param {Options | undefined} [options] - Optional parsing options.
+/// @returns {Array<Statement>} - The parsed SQL statements.
+#[wasm_bindgen(js_name = parseSql, skip_jsdoc)]
+pub fn parse_sql(sql: &str, options: Option<JsOptions>) -> Result<JsValue, JsValue> {
+    let options: Options = match options {
+        None => Options::default(),
+        Some(js_options) => js_options.0,
     };
 
     let vec_statement: Vec<Statement<'_>> = parse_with_options(sql, options).collect();
